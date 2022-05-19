230137749d3e0279f1fd5c33b22ba20fcb4ebf2c
Abu Sufiyan <sufiyansrs@gmail.com>
2022-05-19 16:16:56 +0530

    JSON to Collection Object and Date Object using @RequestBody

---
+++ SpringBootRestProj07-JSONToCollectionObjectsusingRequestBody/src/main/java/com/rop/SpringBootRestProj07JsonToCollectionObjectsusingRequestBodyApplication.java
@@ -0,0 +1,13 @@
+package com.rop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestProj07JsonToCollectionObjectsusingRequestBodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestProj07JsonToCollectionObjectsusingRequestBodyApplication.class, args);
+	}
+
+}
