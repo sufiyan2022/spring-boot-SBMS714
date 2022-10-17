4e38704844f8522a4fc5c9926164f518495a69a4
Abu Sufiyan <sufiyansrs@gmail.com>
2022-10-17 16:45:40 +0530

    change with GET mode request. and some other experiment with @PathVariable

--- SpringBootRestProj09-PathVariables/src/main/java/com/rop/controller/CustomerOperationsController.java
+++ SpringBootRestProj09-PathVariables/src/main/java/com/rop/controller/CustomerOperationsController.java
@@ -7,47 +7,90 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CustomerOperationsController {
 
-//	@PostMapping("/report/{no}/{name}")
+	
+	/*
+	 * experiment 1
+	 */
+//	@GetMapping("/report/{no}/{name}")
 //	public String fetchData2(@PathVariable(name="name")String cname, @PathVariable(required = false) Integer no){
-//		return cname+"\t\t"+no;
+//		return no+"\t\t"+cname;
 //	}
 	
-//	@PostMapping("/report/{no}/{name}")
+	/*
+	 * 1 with parameter String,String
+	 */
+	
+//	@GetMapping("/report/{no}/{name}")
 //	public String fetchData2(@PathVariable(name="name")String cname, @PathVariable(required = false) String no){//In this both are String
-//		return cname+"\t\t"+no;
+//		return no+"\t\t"+cname;
 //	}
 	
 	//---------------------------------------------------------------------
 	
-//	@PostMapping("/report/{no}/{name}")
+	/*
+	 * experiment 2
+	 */
+	
+//	@GetMapping("/report/{no}/{name}")
 //	public String fetchData2(@PathVariable(name="name",required=false)String cname, @PathVariable(required = false) Integer no){
-//		return cname+"\t\t"+no;
+//		return no+"\t\t"+cname;
 //	}
 	
 	//------------------------------------------------------------------------
 	
-	@GetMapping("/report/no/name")
-	public String fetchData1(@PathVariable(name="name",required = false)String cname, @PathVariable(required = false) Integer no){
-		System.out.println("from fetchData1");
-		return cname+"\t\t"+no;
-	}
+	/*
+	 * experiment 3
+	 */
+//	@GetMapping("/report/no/name")
+//	public String fetchData1(@PathVariable(name="name",required = false)String cname, @PathVariable(required = false) Integer no){
+//		System.out.println("from fetchData1");
+//		return no+"\t\t"+cname+"\tfrom fetchData1";
+//	}
+//	
+//	@GetMapping("/report/no/{name}")
+//	public String fetchData2(@PathVariable(name="name",required = false)String cname, @PathVariable(required = false) Integer no){
+//		System.out.println("from fetchData2");
+//		return no+"\t\t"+cname+"\tfrom fetchData2";
+//	}
+//	
+//	@GetMapping("/report/{no}/{name}")
+//	public String fetchData3(@PathVariable(name="name",required = false)String cname, @PathVariable(required = false) Integer no){
+//		System.out.println("from fetchData3");
+//		return no+"\t\t"+cname+"\tfrom fetchData3";
+//		
+//	}
+//	
+//	@GetMapping("/report/{no}/name")
+//	public String fetchData4(@PathVariable(name="name",required = false)String cname, @PathVariable(required = false) Integer no){
+//		System.out.println("from fetchData4");
+//		return no+"\t\t"+cname+"\tfrom fetchData4";
+//	}
 	
-	@GetMapping("/report/no/{name}")
-	public String fetchData2(@PathVariable(name="name",required = false)String cname, @PathVariable(required = false) Integer no){
-		System.out.println("from fetchData2");
-		return cname+"\t\t"+no;
+	//----------------------------------------------------
+	
+	/*
+	 * experiment 4
+	 */
+	
+	@GetMapping("/report/101/raja")
+	public String fetchData1(){
+		System.out.println("from fetchData1");
+		return "from fetchData1";
 	}
 	
 	@GetMapping("/report/{no}/{name}")
-	public String fetchData3(@PathVariable(name="name",required = false)String cname, @PathVariable(required = false) Integer no){
-		System.out.println("from fetchData3");
-		return cname+"\t\t"+no;
+	public String fetchData2(@PathVariable(name="name")String cname, @PathVariable Integer no){
+		System.out.println("from fetchData2");
+		return "from fetchData2";
 	}
 	
-	@GetMapping("/report/{no}/name")
-	public String fetchData4(@PathVariable(name="name",required = false)String cname, @PathVariable(required = false) Integer no){
-		System.out.println("from fetchData4");
-		return cname+"\t\t"+no;
-	}
+	//------------------------------------------------------------------------
+	
+	/*
+	 * experiment 5
+	 * if we take two method with same request path then we well get illegalStateException.
+	 */
+	
+	
 	
 }
